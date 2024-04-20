1e87fea5066575cab8d50f82d99252cbe3d7fe8d
BINAY BHUSAN <mohantabinaybhusan@gmail.com>
2024-04-20 14:17:44 +0530

    code of npm

--- FigletDir/index.js
+++ FigletDir/index.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("Hello World!!", function (err, data) {
+figlet("Binay!!", function (err, data) {
     if (err) {
       console.log("Something went wrong...");
       console.dir(err);
